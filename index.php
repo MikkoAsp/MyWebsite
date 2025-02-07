dc302324773cfe7a4218f7e6c763f9d1ddc98517
Mikko Asp <bmias002@edu.xamk.fi>
2025-02-07 12:41:18 +0200

    https://github.com/MikkoAsp/harjoitus.git

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fi">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Responsiivinen 12-sarakkeinen Layout</title>
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+  <div class="container">
+    <div class="header">Header</div>
+    <div class="navigation">
+      <a href="#">Linkki 1</a>
+      <a href="#">Linkki 2</a>
+      <a href="#">Linkki 3</a>
+    </div>
+    <div class="content1">Sisältö 1</div>
+    <div class="content2">Sisältö 2</div>
+    <div class="footer">
+      <footer>Footer</footer>
+    </div>
+  </div>
+</body>
+
+</html>
